b0135ffdda804f49a8905617ef06dcd4a3956958
priscilacvm <priscilacvm@gmail.com>
2021-01-06 21:41:04 -0300

    Javascript

---
+++ main.js
@@ -0,0 +1,118 @@
+function clicou(){
+//    alert("Obrigada por clicar");
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar(){
+    window.open("https://www.linkedin.com/feed/");
+    window.location.href = "https://www.linkedin.com/feed/";
+}
+
+function trocar(){
+    alert("Trocar texto")
+}
+
+
+/*
+function soma(n1, n2){
+    return n1+n2;
+}
+
+
+var validar = 0;
+
+function validaIdade(idade){
+    if (idade>= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+validaIdade(idade)
+console.log(validar);
+alert(validar)
+
+*/
+
+/*
+function setReplace(frase,nome,novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5,10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+/*
+var d = new Date();
+//alert (d.getMonth() + 1);
+//alert (d.getMinutes());
+alert (d.getDay());
+*/
+
+/*
+var d = new Date();
+alert (d)
+*/
+
+
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count<=5){
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+
+
+//var idade = prompt("Qual a sua idade?");
+//var idade = 18;
+//if(idade>=18){
+ //   alert("maior de idade");
+//}else{
+//    alert("menor de idade");
+//}
+
+
+
+//var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva",cor:"roxa"}]
+//console.log(frutas);
+//alert(frutas[0].cor);
+
+
+//var lista  = ["maçã", "pêra","laranja"];
+//lista.push("banana");
+//lista.pop();
+//console.log(lista)
+//console.log(lista.length)
+//console.log(lista.reverse())
+//console.log(lista.toString())
+//console.log(lista.join(" - "))
+//alert(lista.join(" * "))
+
+
+
+
+
+//var nome = "Priscila Cardoso Vieira de Melo";
+//var idade = 37;
+//var idade2 = 10;
+//var frase = "Argentina é o melhor time do mundo";
+//alert(idade + idade2);
+//alert(nome + " tem " + idade + "anos");
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Argentina","Brasil"));
+//alert(frase.replace("Argentina","Brasil"));
